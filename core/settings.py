a5b311ac6d4d4b26375ea735449db7556d4e6650
afroj-5555 <afrojentrepreneur@gmail.com>
2026-05-19 01:20:46 +0530

    add wsgi application setting

--- core/settings.py
+++ core/settings.py
@@ -68,8 +68,7 @@ TEMPLATES = [
     },
 ]
 
-ASGI_APPLICATION = 'core.asgi.application'
-
+WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
